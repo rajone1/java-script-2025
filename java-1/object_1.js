7a535d4b048a6deda022f39870936c22cadc7e67
rajone1 <rajoneweab101@gmail.com>
2025-10-30 23:34:09 +0600

    object

--- java-1/object_1.js
+++ java-1/object_1.js
@@ -3,12 +3,13 @@
 
 // object literals
 
-
+const mysym = Symbol("shaon1")
 
 
 const jsuser1 = {
 
 name : "Rezwan",
+[mysym]: "R1",
 
 age :18,
 
@@ -20,3 +21,25 @@ friend:["tom","jerry"]
 }
 console.log(jsuser1.name);
 console.log(jsuser1["name"]);
+// use symbol
+console.log(jsuser1[mysym]);
+console.log(jsuser1);
+console.log(typeof jsuser1[mysym]);
+
+///////
+
+jsuser1.email = "rezwan111111111111@gmail"
+
+console.log(jsuser1);
+// dont change value
+// Object.freeze(jsuser1)
+jsuser1.email = "rezwan222222222222@gmail"
+
+console.log(jsuser1);
+
+jsuser1.greetfirst = function(){
+
+    console.log("hello");
+    console.log(`hello ${this.name}`);
+}
+console.log(jsuser1.greetfirst());
